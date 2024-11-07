ea29918f98f33f76eb90120bb664f20eb741ffaf
Achref Chenini <achrefchnenni96@gmail.com>
2024-11-07 19:59:48 +0100

    first commit

---
+++ src/main/java/com/rentcarachref/RentCarAchrefApplication.java
@@ -0,0 +1,13 @@
+package com.rentcarachref;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentCarAchrefApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentCarAchrefApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rentcarachref/RentCarAchrefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentcarachref;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentCarAchrefApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
